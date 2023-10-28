c2074f3116e85d808d3a65f37682945e6445c44a
sharathgitha627 <ADMIN@DESKTOP-S78URNJ>
2023-10-28 11:07:17 +0530

    this is first push....

---
+++ SimpleGitProject/src/com/dss/Demo1.java
@@ -0,0 +1,13 @@
+package com.dss;
+
+public class Demo1 {
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("this is line1....");
+		
+		System.out.println("this is line2.....");
+	}
+
+}
