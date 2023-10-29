7915bb9bd9309c211f6fee0076c2356b0178a10a
sharathgitha627 <sharathgitha1627@gmail.com>
2023-10-29 19:17:45 +0530

    changes done

--- SimpleGitProject/src/com/dss/Demo1.java
+++ SimpleGitProject/src/com/dss/Demo1.java
@@ -5,11 +5,8 @@ public class Demo1 {
 	public static void main(String[] args) {
 		
 		
-
-
 		System.out.println("this is line01....");
-
-		
+	
 		System.out.println("this is line2.....");
 		
 		System.out.println("this is line3....");
@@ -24,7 +21,6 @@ public class Demo1 {
 		
 		System.out.println("this is line 8.....");
 		
-
 		System.out.println("this is line 09....");
 
 		
